fed8fd39673f3d40375ad2e95048f4b6089fa7a7
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-07-16 00:21:32 +0300

    pre-version free template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // burger menu
+  const burger = document.querySelector('.burger');
+  const headerMenu = document.querySelector('.header__menu');
+
+  burger.addEventListener('click', (e) => {
+    burger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+  });
+
+  // swiper
+  // eslint-disable-next-line no-undef
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 60,
+      },
+    },
+  });
+});
